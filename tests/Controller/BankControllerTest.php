98bc9306f077de043798e4e5ff03d7bfb41c3684
Y4ch <patryk.jachowski@gmail.com>
2020-09-10 22:24:17 +0200

    testing configuration

---
+++ tests/Controller/BankControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Controller\BankController;
+use PHPUnit\Framework\TestCase;
+
+class BankControllerTest extends TestCase
+{
+
+    public function testShouldCreateNewBankAndAssignItToLoggedUser()
+    {
+        $this->assertTrue(true);
+    }
+}

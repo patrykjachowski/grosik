3d42af405d04c06872ead019e534c51b6c2726ee
patrykjachowski <patryk.jachowski@gmail.com>
2020-10-09 22:03:47 +0200

    bank crud, testing bank create

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -33,6 +35,16 @@ class User implements UserInterface
      * @ORM\Column(type="string")
      */
     private $password;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Bank::class, mappedBy="user", orphanRemoval=true)
+     */
+    private $banks;
+
+    public function __construct()
+    {
+        $this->banks = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -110,5 +122,36 @@ class User implements UserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    /**
+     * @return Collection|Bank[]
+     */
+    public function getBanks(): Collection
+    {
+        return $this->banks;
+    }
+
+    public function addBank(Bank $bank): self
+    {
+        if (!$this->banks->contains($bank)) {
+            $this->banks[] = $bank;
+            $bank->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBank(Bank $bank): self
+    {
+        if ($this->banks->contains($bank)) {
+            $this->banks->removeElement($bank);
+            // set the owning side to null (unless already changed)
+            if ($bank->getUser() === $this) {
+                $bank->setUser(null);
+            }
+        }
+
+        return $this;
     }
 }
---
+++ tests/Controller/BankControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Controller\BankController;
+use App\Entity\User;
+use App\Form\BankType;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class BankControllerTest extends WebTestCase
+{
+
+    public function testShouldCreateBankByLoggedUser()
+    {
+        // Given
+        $client = static::createClient();
+        $userRepository = static::$container->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('test@test.pl');
+        $bankName = 'Test bank';
+
+        // When
+        $client->loginUser($testUser);
+        $client->request('GET', '/bank/new');
+        $client->submitForm('Save', ['bank[name]' => $bankName]);
+
+        $collection = $testUser->getBanks();
+        $filteredCollection = $collection->filter(function($element, $bankName) {
+            return $element->getName() == $bankName;
+        });
+
+        // Then
+        $this->assertNotEmpty($filteredCollection);
+        $this->assertEquals(1, $filteredCollection->count());
+    }
+
+}

d99f0d6de71ce7437b273bdabf1c6a4cd980bab3
patrykjachowski <patryk.jachowski@gmail.com>
2020-11-02 21:31:03 +0100

    encore, react.js

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+import '../styles/app.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Component from './components/Component'
+
+ReactDOM.render(<Component />, document.getElementById('root'))
+
+console.log('Heloł everybody app.js!');
+
+
+
+

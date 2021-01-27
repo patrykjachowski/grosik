c45bf49574bcca3f0c8a33386979dc6f8e2da5cb
patrykjachowski <patryk.jachowski@gmail.com>
2021-01-27 15:49:34 +0100

    fix subcategory unique

--- migrations/Version20210112130531.php
+++ migrations/Version20210112130531.php
@@ -20,19 +20,16 @@ final class Version20210112130531 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE budget (id INT AUTO_INCREMENT NOT NULL, subcategory_id INT NOT NULL, value DOUBLE PRECISION NOT NULL, date DATE NOT NULL, UNIQUE INDEX UNIQ_73F2F77B5DC6FE57 (subcategory_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE budget (id INT AUTO_INCREMENT NOT NULL, subcategory_id INT NOT NULL, value DOUBLE PRECISION NOT NULL, date DATE NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        //$this->addSql('CREATE TABLE budget (id INT AUTO_INCREMENT NOT NULL, subcategory_id INT NOT NULL, value DOUBLE PRECISION NOT NULL, date DATE NOT NULL, UNIQUE INDEX UNIQ_73F2F77B5DC6FE57 (subcategory_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE budget ADD CONSTRAINT FK_73F2F77B5DC6FE57 FOREIGN KEY (subcategory_id) REFERENCES subcategory (id)');
-        $this->addSql('ALTER TABLE bank ADD type VARCHAR(255) DEFAULT NULL');
-        $this->addSql('ALTER TABLE subcategory ADD CONSTRAINT FK_DDCA44836ABA6B8 FOREIGN KEY (budget_id) REFERENCES budget (id)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_DDCA44836ABA6B8 ON subcategory (budget_id)');
+        //$this->addSql('ALTER TABLE bank ADD type VARCHAR(255) DEFAULT NULL');
+        //$this->addSql('ALTER TABLE subcategory ADD CONSTRAINT FK_DDCA44836ABA6B8 FOREIGN KEY (budget_id) REFERENCES budget (id)');
     }
 
     public function down(Schema $schema) : void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE subcategory DROP FOREIGN KEY FK_DDCA44836ABA6B8');
         $this->addSql('DROP TABLE budget');
-        $this->addSql('ALTER TABLE bank DROP type');
-        $this->addSql('DROP INDEX UNIQ_DDCA44836ABA6B8 ON subcategory');
+        //$this->addSql('ALTER TABLE bank DROP type');
     }
 }
